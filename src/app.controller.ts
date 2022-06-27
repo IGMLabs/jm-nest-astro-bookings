08821115e29437c31bf26212fe0c35f35a051dde
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-06-27 10:52:54 +0200

    params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
@@ -9,4 +9,40 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/test")
+  public getTest(): string {
+    return "Hola Test";
+  }
+
+
+  @Get("/param/:id")
+  public getParam(@Param("id") id: string): string {
+    const type = typeof id;
+    return `Param: ${id} of type ${type}`;
+  }
+
+  @Get("/square/:someParam")
+  public getSquare(@Param("someParam") someParam: number): string{
+    const type = typeof someParam;
+    const square = someParam * someParam;
+    return `Square of ${someParam} is ${square}`;
+  }
+
+  @Get("/square/Nan/:someParam")
+  public getSquarePipe(@Param("someParam") someParam: string): string{
+    const someNumber = parseInt(someParam);
+    if(isNaN(someNumber)) throw new HttpException(`${someParam} is not a number`, HttpStatus.BAD_REQUEST);
+    const type = typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square of ${someNumber} of type ${type} is ${square}`;
+  }
+
+  @Get("/square/pipe/:someParam")
+  public getSquareIntPipe(@Param("someParam", ParseIntPipe) someNumber: number): string{
+    const type = typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square of ${someNumber} of type ${type} is ${square}`;
+  }
+  
 }
