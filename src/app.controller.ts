f458ec752174f4516c3cc313b3cc223a6734f140
juanjo.moure <juanjo.moure@igmweb.com>
2022-06-27 13:10:18 +0200

    added client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Put, HttpException, HttpStatus, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { Client } from "./client.interface";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("")
   public getHello(): string {
     return this.appService.getHello();
   }
 
   @Post("")
-  public postHello(@Body() payload: {name:string}): string {
-    return payload.name;
+  public postHello(@Body() payload: unknown): string {
+    const type = typeof payload;
+    const nameString = JSON.stringify(payload);
+    return `Body: ${payload} of type ${type}; ${nameString}`;
+  }
+
+  @Post("name")
+  public postHelloName(@Body() payload: { name: string }): string {
+    return `Hello ${payload.name}`;
   }
 
   @Get("/test")
@@ -86,6 +94,22 @@ export class AppController {
       return this.appService.raiz( number );
     }catch(error){
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Post("client")
+  public postClient(@Body() payload: Client): Client {
+    return this.appService.saveClient(payload);
+  }
+
+  @Put("client/:id")
+  public putClient(@Param("id") clientId: string, @Body() payload: Client): Client {
+    try {
+      return this.appService.updateClient(clientId, payload);
+    } catch (error) {
+      const message: string = error.message;
+      if (message.startsWith("NOT FOUND:")) throw new HttpException(message, HttpStatus.NOT_FOUND);
+      else throw new HttpException(message, HttpStatus.BAD_REQUEST);
     }
   }
 
