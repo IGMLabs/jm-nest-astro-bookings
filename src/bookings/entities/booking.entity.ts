6c12204d4db6cf18a969987900ec3d0a0f42ac6c
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-07-05 09:54:23 +0200

    bookings

---
+++ src/bookings/entities/booking.entity.ts
@@ -0,0 +1,23 @@
+import { Trip } from "src/trips/entities/trip.entity";
+import { Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
+
+@Entity("bookings")
+export class Booking {
+    @PrimaryColumn()
+    id: string;
+
+    @ManyToOne(() => Trip)
+    trip: Trip;
+
+    @Column({nullable: false})
+    client: string;
+
+    @Column({type: "int", default: 1})
+    passengers: number;
+
+    @Column({ type: "timestamp", default: () => "now()"})
+    createdAt: Date;
+
+    @Column({ nullable: true })
+    updatedAt: Date;
+}
